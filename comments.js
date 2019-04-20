0d4d28f36af415a2e6a198e744328212fb22b865
Tony Comito <ac106378@gmail.com>
2019-04-19 19:07:10 -0700

    Fixed async skipping button listeners
    added an additional promise to ensure the button event listeners are only added once the whole page is completely done loaded.

--- comments.js
+++ comments.js
@@ -37,60 +37,61 @@
 
         postcontainer.appendChild(newPost);
       });
-    });
-  window.onload = function () {
-    // select all buttons with this attr
-    const BUTTON_SELECTOR = '[data-posts="id"]';
-    let buttons = document.querySelectorAll(BUTTON_SELECTOR);
+    })
+    .then(
+      window.onload = function () {
+        // select all buttons with this attr
+        const BUTTON_SELECTOR = '[data-posts="id"]';
+        let buttons = document.querySelectorAll(BUTTON_SELECTOR);
 
-    // for each button matching the selector
-    buttons.forEach(function (button) {
-      'use strict';
+        // for each button matching the selector
+        buttons.forEach(function (button) {
+          'use strict';
 
-      let sectionSelector = `#comments-${button.value}`;
-      let commentSection = document.querySelector(sectionSelector);
+          let sectionSelector = `#comments-${button.value}`;
+          let commentSection = document.querySelector(sectionSelector);
 
-      button.addEventListener('click', function (event) {
-        if (commentSection.hidden) {
-          if (commentSection.hasChildNodes() === false) {
-            // attach header
-            let header = document.createElement('h3');
-            header.innerHTML = 'Comments';
-            commentSection.appendChild(header);
-            // fetch and populate comments
-            console.log('loading from api...');
-            fetch(`https://jsonplaceholder.typicode.com/comments?postId=${button.value}`)
-              .then(response => response.json())
-              .then(function (comments) {
-                comments.forEach(function (comment) {
-                  // create and add comment body
-                  let commentbody = document.createElement('p');
-                  commentbody.setAttribute('data-comments', 'body');
-                  commentbody.innerHTML = comment.body.replace(/\n/g, '<br>');
-                  commentSection.appendChild(commentbody);
+          button.addEventListener('click', function (event) {
+            if (commentSection.hidden) {
+              if (commentSection.hasChildNodes() === false) {
+                // attach header
+                let header = document.createElement('h3');
+                header.innerHTML = 'Comments';
+                commentSection.appendChild(header);
+                // fetch and populate comments
+                console.log('loading from api...');
+                fetch(`https://jsonplaceholder.typicode.com/comments?postId=${button.value}`)
+                  .then(response => response.json())
+                  .then(function (comments) {
+                    comments.forEach(function (comment) {
+                      // create and add comment body
+                      let commentbody = document.createElement('p');
+                      commentbody.setAttribute('data-comments', 'body');
+                      commentbody.innerHTML = comment.body.replace(/\n/g, '<br>');
+                      commentSection.appendChild(commentbody);
 
-                  // attach address element to name
-                  let name = document.createElement('a');
-                  name.setAttribute('data-comments', 'email');
-                  name.setAttribute('href', `mailto:${comment.email}`);
-                  name.innerHTML = comment.name;
+                      // attach address element to name
+                      let name = document.createElement('a');
+                      name.setAttribute('data-comments', 'email');
+                      name.setAttribute('href', `mailto:${comment.email}`);
+                      name.innerHTML = comment.name;
 
-                  let address = document.createElement('address');
-                  address.setAttribute('data-comments', 'name');
-                  address.appendChild(name);
-                  commentSection.appendChild(address);
-                });
-              });
-          }
-          commentSection.hidden = false;
-          button.textContent = 'Hide comments';
-        } else {
-          commentSection.hidden = true;
-          button.textContent = 'Show comments';
-        }
+                      let address = document.createElement('address');
+                      address.setAttribute('data-comments', 'name');
+                      address.appendChild(name);
+                      commentSection.appendChild(address);
+                    });
+                  });
+              }
+              commentSection.hidden = false;
+              button.textContent = 'Hide comments';
+            } else {
+              commentSection.hidden = true;
+              button.textContent = 'Show comments';
+            }
 
-        event.preventDefault();
+            event.preventDefault();
+          });
+        });
       });
-    });
-  };
 })(window);
